c5daa9e3b9b073b0845fb3393d6edb91b2eedb0c
AviralCoder <aviral.kochgaway@gmail.com>
2021-07-30 23:50:49 +0530

    add link to source code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,10 @@ const Navbar = ({ title }) => {
     return (
         <section className="navbar">
             <h1>{title}</h1>
+
+            <a href="https://github.com/AviralCoder/codepen_clone">
+                <button>View On Github</button>
+            </a>
         </section>
     );
 };
